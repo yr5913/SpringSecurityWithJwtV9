ff76057becca59ba81363b7e3098e7d9910eeb57
yr5913 <yr5913@rit.edu>
2023-09-22 20:57:46 -0400

    changed passwordencoder to bean

--- src/main/java/com/simplebank/config/SecurityConfig.java
+++ src/main/java/com/simplebank/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -44,7 +46,10 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
-
+        /*
+       Approach 1:  Using password encoder with each user
+         */
+/*
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("admin")
@@ -53,6 +58,27 @@ public class SecurityConfig {
                 .username("user")
                 .password("12345")
                 .authorities("read").build();
-        return new InMemoryUserDetailsManager(admin);
+*/
+
+        /*
+       Approach 1:  Using password encoder as a bean to use for all users
+         */
+        UserDetails admin = User.withUsername("admin")
+                .password("admin")
+                .authorities("admin").build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read").build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    /**
+     * NoOpPasswordEncoder is for only non-prod
+     *
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
