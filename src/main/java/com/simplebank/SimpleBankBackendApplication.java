8811aa95cdc924b485e1b583fd1c22b3927b0144
yr5913 <yr5913@rit.edu>
2023-09-25 02:12:58 -0400

    Removed spring security debug

--- src/main/java/com/simplebank/SimpleBankBackendApplication.java
+++ src/main/java/com/simplebank/SimpleBankBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
 public class SimpleBankBackendApplication {
 
     public static void main(String[] args) {
